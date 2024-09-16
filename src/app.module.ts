fc171fd52c3ce0cceaa1b5b969edc553735d78ad
German Alexis <alexisdager93@hotmail.com>
2024-09-16 12:12:16 -0500

    feature: added module mongoose way async

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongooseModuleAsyncOptions } from './mongoose/mongoose.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+    MongooseModule.forRootAsync(mongooseModuleAsyncOptions),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
